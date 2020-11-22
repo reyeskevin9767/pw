0d2b93a37139a8cd0166ca1a3f12c5947034467a
Kevin Reyes <reyeskevin9767@gmail.com>
2020-11-22 18:19:04 -0500

    add event handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
+    console.log(`
+    About to gernerate a password with the following: 
+    Includes letters: ${this.includeLetters}
+    Includes Numbers: ${this.includeNumbers}
+    Includes Symbols: ${this.includeSymbols}`)
     this.password = 'My Password!!';
   }
 }
